96a3021dcad9116afbcd6f338fe982386985f92c
capmahin <mahinulhuque1996@gmail.com>
2022-04-11 04:39:40 +0600

    add copmoinets

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        {" "}
+        <small>CopyRight@</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
