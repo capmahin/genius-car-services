d1e7663c6efb9abe40fd0c7205e9d4f1b96408c4
capmahin <mahinulhuque1996@gmail.com>
2022-04-11 05:42:22 +0600

    add a useState and useEffect and distracture it and map it

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import repair1 from "../../../images/repair1.jpg";
-const services = [
-  { id: 1, name: "oil change", price: 100, description: "", img: repair1 },
-];
+import Service from "../Service/Service";
+
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div>
-      <h2>services:</h2>
+      <h2>services:{services.length}</h2>
+      {services.map((service) => (
+        <Service key={service.id} service={service}></Service>
+      ))}
     </div>
   );
 };
