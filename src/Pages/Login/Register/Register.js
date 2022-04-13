d174951c0b122773671f30d25216885ca92e302a
capmahin <mahinulhuque1996@gmail.com>
2022-04-13 17:38:15 +0600

    add requirdQuth

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import "./Register.css";
+import auth from "../../../firebase.init";
 
 const Register = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
   const navigate = useNavigate();
   const navigateLogin = () => {
     navigate("/login");
   };
+
+  if (user) {
+    navigate("/home");
+  }
 
   const handleRegister = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const name = event.target.name.value;
     const password = event.target.name.value;
+
+    createUserWithEmailAndPassword(email, password);
   };
   return (
     <div className="register-form">
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2W3ID4cKsi2ePhl2apA9qsqHTGFRZCXA",
-  authDomain: "genius-car-services-dd221.firebaseapp.com",
-  projectId: "genius-car-services-dd221",
-  storageBucket: "genius-car-services-dd221.appspot.com",
-  messagingSenderId: "682455612710",
-  appId: "1:682455612710:web:b19bac52044ce495d795e7",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
