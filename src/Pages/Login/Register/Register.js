fcaa48f27afd13dad865463789536dd7952528c0
capmahin <mahinulhuque1996@gmail.com>
2022-04-14 06:03:45 +0600

    add a checkBox and handle Error

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -19,9 +19,10 @@ const Register = () => {
 
   const handleRegister = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
     const name = event.target.name.value;
-    const password = event.target.name.value;
+    const email = event.target.email.value;
+
+    const password = event.target.password.value;
 
     createUserWithEmailAndPassword(email, password);
   };
@@ -46,13 +47,21 @@ const Register = () => {
           placeholder="Your Password"
           required
         />
-        <input type="submit" value="Register" />
+
+        <input type="checkbox" name="terms" id="terms" />
+        <label htmlFor="terms">Accept Genius car terms and condition</label>
+
+        <input
+          className="w-50 mx-auto btn btn-primary mt-2"
+          type="submit"
+          value="Register"
+        />
       </form>
       <p>
         Already have an account ?
         <Link
           to="/login"
-          className="text-danger pe-auto text-decoration-none"
+          className="text-primary pe-auto text-decoration-none"
           onClick={navigateLogin}
         >
           Please Login
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -17,12 +17,10 @@ const SocialLogin = () => {
 
   if (error || error1) {
     errorElement = (
-      <div>
-        <p className="text-danger">
-          Error: {error?.message}
-          {error1?.message}
-        </p>
-      </div>
+      <p className="text-danger">
+        Error: {error?.message}
+        {error1?.message}
+      </p>
     );
   }
 
